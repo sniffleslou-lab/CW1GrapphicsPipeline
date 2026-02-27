197a14628fe2bbdcf1b746a783f2bc5f86f41ba0
FeintWitch <sniffleslou@gmail.com>
2026-02-27 10:15:50 +0000

    added my lab 3 code and tweaked a small stuff

--- Lab 1/scenebasic_uniform.cpp
+++ Lab 1/scenebasic_uniform.cpp
@@ -3,7 +3,11 @@
 #include <cstdio>
 #include <cstdlib>
 
+#include <sstream>
+
 #include <string>
+#include <filesystem>
+
 using std::string;
 
 #include <iostream>
@@ -12,70 +16,52 @@ using std::endl;
 
 #include "helper/glutils.h"
 
+#include <glm/gtc/matrix_transform.hpp>
 using glm::vec3;
+using glm::mat4; //lab2
+using glm::vec4;
 
-SceneBasic_Uniform::SceneBasic_Uniform() : angle(0.0f) {}
+#include <glm/gtc/constants.hpp>
+
+
+//below used to be angl(0.0f)
+
+SceneBasic_Uniform::SceneBasic_Uniform() : plane (10.0f, 10.0f, 100, 100) {
+
+    mesh = ObjMesh::load("media/pig_triangulated.obj", true);
+}
 
 void SceneBasic_Uniform::initScene()
 {
     compile();
 
-    std::cout << std::endl;
+   
+    glEnable(GL_DEPTH_TEST);
+    //set the camera and the 3 lights 
+    view = glm::lookAt(vec3(3.6f, 0.75f, 0.75f),
+        vec3(0.0f, 0.1f, 0.0f),
+        vec3(0.0f, 1.0f, 0.0f));
+    projection = mat4(1.0f);
+    // Single light for per-fragment Phong
+    vec3 lightPosEye = vec3(view * vec4(-2.0f, 4.0f, 2.0f, 1.0f));
+    prog.setUniform("LightPosition", lightPosEye);
+    prog.setUniform("LightLa", vec3(0.2f));
+    prog.setUniform("LightLd", vec3(0.8f));
+    prog.setUniform("LightLs", vec3(0.8f));
+    // specular
 
-    prog.printActiveUniforms();
-
-    /////////////////// Create the VBO ////////////////////
-    float positionData[] = {
-        -0.8f, -0.8f, 0.0f,
-         0.8f, -0.8f, 0.0f,
-         0.0f,  0.8f, 0.0f };
-    float colorData[] = {
-        1.0f, 0.0f, 0.0f,
-        0.0f, 1.0f, 0.0f,
-        0.0f, 0.0f, 1.0f };
-
-    // Create and populate the buffer objects
-    GLuint vboHandles[2];
-    glGenBuffers(2, vboHandles);
-    GLuint positionBufferHandle = vboHandles[0];
-    GLuint colorBufferHandle = vboHandles[1];
-
-    glBindBuffer(GL_ARRAY_BUFFER, positionBufferHandle);
-    glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), positionData, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ARRAY_BUFFER, colorBufferHandle);
-    glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), colorData, GL_STATIC_DRAW);
-
-    // Create and set-up the vertex array object
-    glGenVertexArrays( 1, &vaoHandle );
-    glBindVertexArray(vaoHandle);
-
-    glEnableVertexAttribArray(0);  // Vertex position
-    glEnableVertexAttribArray(1);  // Vertex color
-
-    #ifdef __APPLE__
-        glBindBuffer(GL_ARRAY_BUFFER, positionBufferHandle);
-        glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 0, (GLubyte *)NULL );
-
-        glBindBuffer(GL_ARRAY_BUFFER, colorBufferHandle);
-        glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, 0, (GLubyte *)NULL );
-    #else
-    		glBindVertexBuffer(0, positionBufferHandle, 0, sizeof(GLfloat)*3);
-    		glBindVertexBuffer(1, colorBufferHandle, 0, sizeof(GLfloat)*3);
-
-    		glVertexAttribFormat(0, 3, GL_FLOAT, GL_FALSE, 0);
-    		glVertexAttribBinding(0, 0);
-    		glVertexAttribFormat(1, 3, GL_FLOAT, GL_FALSE, 0);
-    	  glVertexAttribBinding(1, 1);
-    #endif
-    glBindVertexArray(0);
+  
 }
 
 void SceneBasic_Uniform::compile()
 {
+    std::cout << "Working directory: " << std::filesystem::current_path() << std::endl;
 	try {
-		prog.compileShader("shader/basic_uniform.vert");
-		prog.compileShader("shader/basic_uniform.frag");
+      
+
+        prog.compileShader("shader/PerFragment_phong.vert");
+        prog.compileShader("shader/PerFragment_phong.frag");
+
 		prog.link();
 		prog.use();
 	} catch (GLSLProgramException &e) {
@@ -87,23 +73,58 @@ void SceneBasic_Uniform::compile()
 void SceneBasic_Uniform::update( float t )
 {
 	//update your angle here
+  //  if (m_animate) {
+    //    angle += 0.1f;
+      //  if (angle >= 360.0f) angle -= 360.0f;
+    //}
+
+   
+
 }
 
 void SceneBasic_Uniform::render()
 {
-    glClear(GL_COLOR_BUFFER_BIT);
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     
-    //create the rotation matrix here and update the uniform in the shader 
+    prog.setUniform("Kd", 1.0f, 0.6f, 0.7f);
+    prog.setUniform("Ks", 1.0f, 1.0f, 1.0f);
+    prog.setUniform("Ka", 0.6f, 0.3f, 0.4f);
+    prog.setUniform("Shininess", 200.0f);
 
-    glBindVertexArray(vaoHandle);
-    glDrawArrays(GL_TRIANGLES, 0, 3 );
 
-    glBindVertexArray(0);
+    model = mat4(1.0f);
+    model = glm::rotate(model, glm::radians(90.0f), vec3(0.0f, 1.0f, 0.0f));
+    setMatrices();
+    mesh->render();
+
+
+    prog.setUniform("Kd", 0.4f, 0.4f, 0.4f);
+    prog.setUniform("Ks", 0.9f, 0.9f, 0.9f);
+    prog.setUniform("Ka", 0.5f, 0.5f, 0.5f);
+    prog.setUniform("Shininess", 180.0f);
+
+
+    model = mat4(1.0f);
+    model = glm::translate(model, vec3(0.0f, -0.45f, 0.0f));
+    setMatrices();
+    plane.render();
+}
+
+void SceneBasic_Uniform::setMatrices() 
+{
+    mat4 mv = view * model;
+    prog.setUniform("ModelViewMatrix", mv);
+    prog.setUniform("NormalMatrix", 
+        glm::mat3(vec3(mv[0]), vec3(mv[1]), vec3(mv[2])));
+    prog.setUniform("MVP", projection * mv);
 }
 
 void SceneBasic_Uniform::resize(int w, int h)
 {
-    width = w;
-    height = h;
+  //  width = w;
+  //  height = h;
     glViewport(0,0,w,h);
+    float aspect = (float)w / h;
+    projection = glm::perspective(glm::radians(60.0f), aspect, 0.3f, 100.0f);
 }
--- Lab 1/scenebasic_uniform.h
+++ Lab 1/scenebasic_uniform.h
@@ -6,12 +6,36 @@
 #include <glad/glad.h>
 #include "helper/glslprogram.h"
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+//lab 2 work
+//#include "helper/torus.h"
+
+//lab3 work
+#include "helper/plane.h"
+#include "helper/objmesh.h"
+
 class SceneBasic_Uniform : public Scene
 {
 private:
-    GLuint vaoHandle;
+    //GLuint vaoHandle; LAB1 LINE
+   // Torus torus; 
+
+    //lab3 plane and objmesh
+    Plane plane; //plane surface
+    std::unique_ptr<ObjMesh>mesh; //pig mesh
+
     GLSLProgram prog;
-    float angle;
+
+
+    glm::mat4 model;
+    glm::mat4 view;
+    glm::mat4 projection;
+
+  //  float angle;LAB1 LINE
+    //glm::mat4 rotationMatrix;LAB1 LINE
+
 
     void compile();
 
@@ -22,6 +46,9 @@ public:
     void update( float t );
     void render();
     void resize(int, int);
+
+    void setMatrices();//lab2 
 };
 
 #endif // SCENEBASIC_UNIFORM_H
+
